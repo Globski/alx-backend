8f5bc76a94a6ae54a59cd76904be0872df29a680
Gloriana <141042662+Globski@users.noreply.github.com>
2024-11-03 08:06:55 -0800

    Created MRUCache class that inherits from BaseCaching.

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 
 """ MRUCache module
 """
 from base_caching import BaseCaching
@@ -12,6 +12,7 @@ class MRUCache(BaseCaching):
         """ Initialize the MRUCache
         """
         super().__init__()
+        self.order = []
 
     def put(self, key, item):
         """ Add an item to the cache
@@ -25,12 +26,19 @@ class MRUCache(BaseCaching):
         if key is None or item is None:
             return
 
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.order.remove(key)
+            self.order.append(key)
+            return
+
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            most_recent_key = list(self.cache_data.keys())[-1]
+            most_recent_key = self.order.pop()
             print(f"DISCARD: {most_recent_key}")
             del self.cache_data[most_recent_key]
 
         self.cache_data[key] = item
+        self.order.append(key)
 
     def get(self, key):
         """ Get an item from the cache
@@ -42,4 +50,8 @@ class MRUCache(BaseCaching):
         """
         if key is None or key not in self.cache_data:
             return None
+
+        self.order.remove(key)
+        self.order.append(key)
+        
         return self.cache_data[key]
